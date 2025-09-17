6a09e4b33eb6349109b1d7e85b79448119923e44
Clueeng <clueeng@gmail.com>
2025-09-17 13:22:22 +0200

    reorganisation des fichiers

---
+++ scripts/parallax.js
@@ -0,0 +1,16 @@
+(function() {
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#Firstpage");
+
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.001}% ${50 - (_mouseY - _h) * 0.001}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.002}% ${50 - (_mouseY - _h) * 0.002}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.006}% ${50 - (_mouseY - _h) * 0.006}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        elem.style.backgroundPosition = x;
+    }
+})();
