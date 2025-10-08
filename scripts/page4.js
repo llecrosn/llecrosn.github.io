bc5ce7e5ed4ede8d31458a32063b4fa4599a9b09
Landry Lecrosnier <landrylecrosnier@gmail.com>
2025-10-08 13:17:33 +0200

    Update page4.js

--- scripts/page4.js
+++ scripts/page4.js
@@ -1,4 +1,4 @@
-const piramid = document.querySelector('.piramid');
+const cube = document.querySelector('.cube');
 let isDragging = false;
 let startX, startY, currentX = 0, currentY = 0;
 
@@ -7,18 +7,18 @@ const onMouseDown = (e) => {
     isDragging = true;
     startX = e.clientX - currentX;
     startY = e.clientY - currentY;
-    piramid.style.cursor = 'grabbing';
+    cube.style.cursor = 'grabbing';
 };
 const onMouseMove = (e) => {
     if (!isDragging) return;
     currentX = e.clientX - startX;
     currentY = e.clientY - startY;
-    piramid.style.transform = `rotateX(${ -currentY / 5 }deg) rotateY(${ currentX / 5 }deg)`;
+    cube.style.transform = `rotateY(${ currentX / 5 }deg)`;
 };
 const onMouseUp = () => {
     isDragging = false;
-    piramid.style.cursor = 'grab';
+    cube.style.cursor = 'grab';
 };
-piramid.addEventListener('mousedown', onMouseDown);
+cube.addEventListener('mousedown', onMouseDown);
 window.addEventListener('mousemove', onMouseMove);
 window.addEventListener('mouseup', onMouseUp);
